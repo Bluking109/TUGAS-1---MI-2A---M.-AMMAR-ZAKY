2cb04d1d121bfa3b9b31fd8e4cceef122fa0c956
MUHAMMAD AMMAR ZAKY <222300027@polman.astra.ac.id>
2024-06-21 15:21:15 +0700

    push

---
+++ tugas1_agit_ammarzaky-main/src/main/java/id/co/astratech/tugas1_agit_ammarzaky/Tugas1AgitAmmarZakyiApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_ammarzaky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1AgitAmmarZakyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas1AgitAmmarZakyiApplication.class, args);
+    }
+
+}
---
+++ tugas1_agit_ammarzaky-main/src/test/java/id/co/astratech/tugas1_agit_ammarzaky/Tugas1AgitAmmarZakyiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_ammarzaky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1AgitAmmarZakyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
